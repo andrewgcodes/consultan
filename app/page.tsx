6a73d52c1a4a52a4eb4cc7ce1596bddad470b0ed
Andrew <olafblitz@gmail.com>
2023-08-20 17:32:38 -0700

    color changes

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
             className="relative md:ml-[-10px] md:mb-[37px] font-extrabold text-[16vw] md:text-[130px] font-inter text-[#1E2B3A] leading-[0.9] tracking-[-2px] z-[100]"
           >
             Practice for <br />
-            consulting<span className="text-[#407BBF]">interviews</span>
+            consulting <span className="text-[#407BBF]">interviews</span>
             <span className="font-inter text-[#407BBF]">.</span>
           </motion.h1>
           <motion.div
